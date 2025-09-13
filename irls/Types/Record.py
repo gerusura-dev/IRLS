f97c403bf70894477f499f9d939da28c345a93d4
gerusuraimu <gerusuraimu@mail.gerusuraimu.com>
2025-09-13 23:00:52 +0900

    Theta実装

--- irls/Types/Record.py
+++ irls/Types/Record.py
@@ -1,9 +1,10 @@
 from typing import List, Optional
+import math
 from irls.Types import Run, Rect, Point
 
 
 class Record:
-    __slots__ = ("label", "rect", "area", "cor", "cog")
+    __slots__ = ("label", "rect", "area", "cor", "cog", "theta")
 
     def __init__(self, runs: List[Run]) -> None:
         self.label: Optional[int] = runs[0].label if runs else None
@@ -11,9 +12,10 @@ class Record:
         self.area: int = self.__get_area(runs)
         self.cor: Point = self.__get_center_of_rect(self.rect)
         self.cog: Point = self.__get_center_of_gravity(runs)
+        self.theta: float = self.__get_theta(runs, self.cog)
 
     def __repr__(self) -> str:
-        return f"Blob(label={self.label}, rect={self.rect}, area={self.area}, cor={self.cor})"
+        return f"Blob(label={self.label}, rect={self.rect}, area={self.area}, cor={self.cor}, cog={self.cog}, theta={self.theta})"
 
     @staticmethod
     def __get_rect(runs: List[Run]) -> Rect:
@@ -34,4 +36,28 @@ class Record:
 
     @staticmethod
     def __get_center_of_gravity(runs: List[Run]) -> Point:
-        pass
+        x, y = 0, 0
+        length = 0
+
+        for run in runs:
+            x += sum(list(range(run.xs, run.xe + 1)))
+            y += run.y * len(run)
+            length += len(run)
+
+        return Point(x / length + 0.5, y / length + 0.5)
+
+    @staticmethod
+    def __get_theta(runs: List[Run], cog: Point) -> float:
+        m11 = 0
+        m20 = 0
+        m02 = 0
+
+        for run in runs:
+            m11 += sum([(pos + 0.5 - cog.x) * (run.y + 0.5 - cog.y) for pos in range(run.xs, run.xe + 1)])
+            m20 += sum([(pos + 0.5 - cog.x) * (pos + 0.5 - cog.x) for pos in range(run.xs, run.xe + 1)])
+            m02 += sum([(run.y + 0.5 - cog.y) * (run.y + 0.5 - cog.y) for _ in range(run.xs, run.xe + 1)])
+
+        if m11 + m20 + m02 == 0:
+            return 0
+
+        return math.atan((2 * m11) / (m20 - m02))
